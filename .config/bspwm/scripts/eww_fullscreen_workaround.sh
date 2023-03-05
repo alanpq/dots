1010a7f64cb481e89b10fc025692f54138670b07
Alan Panayotov <alanpanayotov@gmail.com>
2023-03-05 18:03:51 +0000

    fix: eww workaround script (hack)

--- .config/bspwm/scripts/eww_fullscreen_workaround.sh
+++ .config/bspwm/scripts/eww_fullscreen_workaround.sh
@@ -4,8 +4,8 @@
 # FIXME: actually support multi monitor stuff
 
 declare -A bars
-bars[0]="top"
-bars[1]="bottom"
+bars[0]="eww-bar-top"
+bars[1]="eww-bar-bottom"
 
 declare -A monitors
 monitors[0x00400006]="eDP-1"
@@ -13,24 +13,30 @@ monitors[0x00400006]="eDP-1"
 declare -A bar_idxs
 counter=0
 for i in "${!monitors[@]}"; do
-  bar_idxs[$monitors[$i]}]=""
+  bar_idxs[${monitors[$i]}}]=""
   #bar_idxs[${monitors[$i]}]="$counter"
   ((counter=counter+1))
 done
 
-echo ${bars[@]}
-echo ${monitors[@]}
-echo ${bar_idxs[@]}
+echo bars: ${bars[@]}
+echo monitors: ${monitors[@]}
+echo bar_idxs: ${bar_idxs[@]}
 
 bspc subscribe node_state | while read -r _ mon _ _ state flag; do
   if [[ "$state" != fullscreen ]]; then continue; fi
+  echo "===== fullscreen change ===="
   for bar in "${bars[@]}"; do 
+    echo "BAR '$bar'"
     if [[ "$flag" == on ]]; then
-      echo "mon: $mon"
-      echo "$bar${bar_idxs[${monitors[$mon]}]}"
-      xdotool search --class "$bar${bar_idxs[${monitors[$mon]}]}" windowunmap
+      echo "  mon: $mon"
+      echo "  bar: $bar${bar_idxs[${monitors[$mon]}]}"
+      xdotool search --class $bar windowunmap
+      #xdotool search --class "$bar${bar_idxs[${monitors[$mon]}]}" windowunmap
     else
-      xdotool search --class "$bar${bar_idxs[${monitors[$mon]}]}" windowmap
+      echo "  mon: $mon"
+      echo "  bar: $bar${bar_idxs[${monitors[$mon]}]}"
+      xdotool search --class $bar windowmap
+      #xdotool search --class "$bar${bar_idxs[${monitors[$mon]}]}" windowmap
     fi
   done
 done
