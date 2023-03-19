31dc586b6b863e46d5b6ed1fff35a30e29a3168e
oofsauce <alanpanayotov@gmail.com>
2023-03-19 02:20:55 +0000

    feat: add eww to reload script

--- .script/reload.sh
+++ .script/reload.sh
@@ -1,4 +1,4 @@
-CHOICE=$(echo $'sxhkd\npolybar\nspotifyd\ndunst\nalbert' | dmenu -i)
+CHOICE=$(echo $'sxhkd\nbspc\neww\npolybar\nspotifyd\ndunst\nalbert' | dmenu -i)
 
 
 case "$CHOICE" in
@@ -6,11 +6,17 @@ case "$CHOICE" in
 		echo "Selection cancelled. Aborting.."
 		exit
 	;;
+  "bspc")
+    exec $HOME/.config/bspwm/bspc &
+  ;;
 	"polybar")
 		killall polybar
 		rm /tmp/POLYBAR_*
 		exec $HOME/.script/launch_polybar.sh &>/tmp/polybar.log &
 	;;
+  "eww")
+    exec $HOME/.config/bspwm/scripts/open_eww.sh &
+  ;;
 	"spotifyd")
 		systemctl --user restart spotifyd
 	;;
