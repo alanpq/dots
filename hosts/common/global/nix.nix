d90dcead408a447d102c65867aee820958023a6a
Alan Panayotov <alanpanayotov@gmail.com>
2024-01-11 13:08:04 +0000

    wip: port zwei-pc config

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -8,6 +8,9 @@
       warn-dirty = false;
       system-features = [ "kvm" "big-parallel" "nixos-test" ];
       flake-registry = ""; # Disable global flake registry
+
+      substituters = [ "https://nix-gaming.cachix.org" ];
+      trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
     };
     gc = {
       automatic = true;
