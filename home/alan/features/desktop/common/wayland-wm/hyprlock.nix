ff29afd9a4815cc289d0e356a0cb8956990994df
Alan Panayotov <alanpanayotov@gmail.com>
2024-09-12 22:31:30 +0100

    fix: hyprlock style improvement

--- home/alan/features/desktop/common/wayland-wm/hyprlock.nix
+++ home/alan/features/desktop/common/wayland-wm/hyprlock.nix
@@ -1,7 +1,7 @@
 { config, pkgs, inputs, ... }:
 let
   inherit (config.colorscheme) colors;
-  inherit (inputs.nix-colors.lib.conversions) hexToRGBString; 
+  inherit (inputs.nix-colors.lib.conversions) hexToRGBString;
 
   color = {c, alpha ? 1.0}: "rgba(${hexToRGBString "," c},${toString alpha})";
 in {
@@ -23,47 +23,64 @@ in {
           vibrancy_darkness = 0.0;
         }
       ];
+      image = [
+        {
+          path = "${../avatar.png}";
+          size = 120;
+          rounding = -1;
+          position = "0, 0";
+          halign = "center";
+          valign = "center";
+
+          border_size = 1;
+          border_color = color {c=colors.base01;};
+        }
+      ];
       input-field = [
         {
-          size = "250, 50";
-          outline_thickness = 3;
+          size = "200, 35";
+          outline_thickness = 1;
           dots_size = 0.33;
           dots_spacing = 0.15;
           dots_center = true;
           outer_color = color {c=colors.base01;};
           inner_color = color {c=colors.base00;};
           font_color = color {c=colors.base07; alpha=0.5;};
-          fail_color = color {c=colors.base08;};
-          placeholder_text = "password";
+          check_color = color {c=colors.base09;};
+          fail_color = color {c=colors.base0C;};
+          placeholder_text = "enter password";
           font_family = "FiraSans Light";
           fade_on_empty = false;
           hide_input = false;
-          position = "0, 200";
+          position = "0, -100";
           halign = "center";
-          valign = "bottom";
+          valign = "center";
         }
       ];
 
       label = [
-        {
+        { # HH:MM
           text = "cmd[update:60000] echo \"<b><big> $(date +\"%H:%M\") </big></b>\"";
           color = color {c = colors.base07; alpha = 0.7;};
-          font_size = 94;
+          font_size = 110;
           font_family = "FiraSans Regular";
 
           position = "0, -150";
           halign = "center";
           valign = "top";
         }
-        {
+        { # Date
           text = "cmd[update:18000000] echo \"\"$(date +'%A, %-d %B %Y')\"\"";
           color = color {c = colors.base07; alpha = 0.7;};
-          font_size = 18;
+          font_size = 20;
           font_family = "FiraSans Light";
 
-          position = "0, -285";
+          position = "0, -300";
           halign = "center";
           valign = "top";
+        }
+        {
+
         }
       ];
     };
