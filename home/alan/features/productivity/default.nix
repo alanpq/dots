c69e04a8c89f535e361de4cf03016b1abbbc0f54
Alan Panayotov <alanpanayotov@gmail.com>
2024-12-11 16:41:46 +0000

    pkgs: add qalculate-gtk

--- home/alan/features/productivity/default.nix
+++ home/alan/features/productivity/default.nix
@@ -1,8 +1,12 @@
-{
+{pkgs, ...}: {
   imports = [
     ./nextcloud-pwd.nix
     ./keepassxc.nix
     ./obsidian.nix
     ./mail.nix
+  ];
+
+  home.packages = with pkgs; [
+    qalculate-gtk
   ];
 }
