007676756b09e172f6bc9d3d283efb6bea099403
Alan Panayotov <alanpanayotov@gmail.com>
2024-12-11 16:44:08 +0000

    gamer-think: add bluetooth support

--- hosts/gamer-think/default.nix
+++ hosts/gamer-think/default.nix
@@ -1,4 +1,8 @@
-{ pkgs, inputs, ... }: {
+{
+  pkgs,
+  inputs,
+  ...
+}: {
   imports = [
     inputs.hardware.nixosModules.common-cpu-intel
     inputs.hardware.nixosModules.common-gpu-intel
@@ -10,17 +14,19 @@
     ../common/optional/docker.nix
     ../common/optional/wireless.nix
     ../common/optional/lightd.nix
-    ../common/optional/pipewire.nix
+
+    ../common/optional/pipewire
+    ../common/optional/pipewire/bluetooth.nix
+
     ../common/optional/gamemode.nix
-    
+
     ../common/users/alan
   ];
-
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.supportedFilesystems = [ "ntfs" "vfat" "ext4" "lvm" ];
+  boot.supportedFilesystems = ["ntfs" "vfat" "ext4" "lvm"];
   boot.initrd.luks.devices.cryptroot.device = "/dev/disk/by-uuid/2acacb9f-fe23-4c80-bf96-695fe89adbf3";
 
   fileSystems."/" = pkgs.lib.mkForce {
@@ -39,7 +45,6 @@
     dconf.enable = true;
     kdeconnect.enable = true;
   };
-
 
   hardware = {
     opengl.enable = true;
