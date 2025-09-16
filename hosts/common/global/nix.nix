2649902a8adb408efc474dfe20a2f9a1f67cec89
Alan Panayotov <alanpanayotov@gmail.com>
2025-09-16 20:44:59 +0100

    feat: add explicit nix-community.cachix/cache.nixos substituters

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -12,8 +12,16 @@
       system-features = ["kvm" "big-parallel" "nixos-test"];
       flake-registry = ""; # Disable global flake registry
 
-      substituters = ["https://nix-gaming.cachix.org"];
-      trusted-public-keys = ["nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="];
+      substituters = [
+        "https://nix-community.cachix.org?priority=1"
+        "https://nix-gaming.cachix.org?priority=2"
+        "https://cache.nixos.org?priority=3"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      ];
     };
     gc = {
       automatic = true;
