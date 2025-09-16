b53b2ca92cdc357c40e32738a9cb8f11562240ce
Alan Panayotov <alanpanayotov@gmail.com>
2025-09-16 20:43:51 +0100

    git: disable insteadOf ssh rewrite

--- home/alan/features/cli/git.nix
+++ home/alan/features/cli/git.nix
@@ -1,4 +1,9 @@
-{ pkgs, lib, config, ... }: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
   programs.git = {
     enable = true;
     package = pkgs.gitAndTools.gitFull;
@@ -6,16 +11,15 @@
       ff = "merge --ff-only";
       fast-forward = "merge --ff-only";
       pushall = "!git remote | xargs -L1 git push --all";
-      lg =
-        "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+      lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
       graph = "log --decorate --oneline --graph";
       add-nowhitespace = "!git diff -U0 -w --no-color | git apply --cached --ignore-whitespace --unidiff-zero -";
     };
     userName = "Alan Panayotov";
-    userEmail = lib.concatStrings [ "alan" "panayotov" "@" "gmail" ".com" ];
+    userEmail = lib.concatStrings ["alan" "panayotov" "@" "gmail" ".com"];
     extraConfig = {
-      core = { editor = "${pkgs.vim}/bin/vim"; };
-      url."git@github.com:".insteadOf = "https://github.com/";
+      core = {editor = "${pkgs.vim}/bin/vim";};
+      # url."git@github.com:".insteadOf = "https://github.com/";
       commit = {
         gpgsign = true;
       };
@@ -25,6 +29,6 @@
       };
     };
     lfs.enable = true;
-    ignores = [ ".direnv" "result" ];
+    ignores = [".direnv" "result"];
   };
 }
