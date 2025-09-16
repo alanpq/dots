695bceebc6afc5fe0392ec459b79ce5093cb0c18
Alan Panayotov <alanpanayotov@gmail.com>
2025-09-16 20:45:20 +0100

    feat: update nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "my dots :)";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/734386e44a79717a89613654a9762aa37d1d4fdf";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
 
