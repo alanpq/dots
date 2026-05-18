e24957dd28e868119db051b0e0beb0636c62584a
Alan Panayotov <alanpanayotov@gmail.com>
2026-05-18 18:16:50 +0100

    add work attic cache

--- hosts/common/global/nix.nix
+++ hosts/common/global/nix.nix
@@ -19,6 +19,7 @@
         "https://hyprland.cachix.org?priority=5"
         "https://walker.cachix.org?priority=6"
         "https://walker-git.cachix.org?priority=7"
+        "http://attic.coralos.ai/coral?priority=8"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
@@ -27,7 +28,9 @@
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
         "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
+        "coral:aW7JkQ+fTF3TRNp0oTSVB66QBmZx5e2f8uJYDkmhbMI="
       ];
+      netrc-file = "/home/alan/.config/nix/netrc";
     };
     gc = {
       automatic = true;
