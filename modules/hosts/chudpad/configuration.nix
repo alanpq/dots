1d9b97cb1e35020a1a35d6fe59d18fb149975c3a
Alan Panayotov <alanpanayotov@gmail.com>
2026-07-18 12:23:28 +0100

    wip

--- modules/hosts/chudpad/configuration.nix
+++ modules/hosts/chudpad/configuration.nix
@@ -1,9 +1,21 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
   flake.modules.nixos.chudpad = {
     imports = with inputs.self.modules.nixos; [
       system-laptop
       systemd-boot
       # bluetooth
     ];
+
+    networking = {
+      hostName = "chudpad";
+      useDHCP = lib.mkDefault true;
+      dhcpcd.IPv6rs = false;
+    };
+
+    system.stateVersion = lib.mkForce "24.05";
   };
 }
--- modules/hosts/chudpad/filesystem.nix
+++ modules/hosts/chudpad/filesystem.nix
@@ -1,9 +1,17 @@
 {
   flake.modules.nixos.chudpad = {
+    boot = {
+      supportedFilesystems = ["ntfs" "vfat" "ext4" "lvm" "btrfs"];
+      initrd.luks.devices.cryptroot.device = "/dev/disk/by-uuid/2acacb9f-fe23-4c80-bf96-695fe89adbf3";
+
+      loader.efi.canTouchEfiVariables = true;
+    };
+
     fileSystems."/" = {
-      device = "/dev/disk/by-uuid/771a526d-e37c-490e-b199-0501dd520f6f";
+      device = "/dev/pool/root";
       fsType = "ext4";
     };
+    swapDevices = [{device = "/dev/pool/swap";}];
 
     fileSystems."/boot" = {
       device = "/dev/disk/by-uuid/5CDB-4FCB";
@@ -11,8 +19,8 @@
       options = ["fmask=0022" "dmask=0022"];
     };
 
-    swapDevices = [
-      {device = "/dev/disk/by-uuid/a4296477-a6fd-4c4b-817f-d7da97dcd9f7";}
-    ];
+    # swapDevices = [
+    #   {device = "/dev/disk/by-uuid/a4296477-a6fd-4c4b-817f-d7da97dcd9f7";}
+    # ];
   };
 }
