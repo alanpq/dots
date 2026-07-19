9a2335d6886f4625136a1bcf2b5980bbadd61178
Alan Panayotov <alanpanayotov@gmail.com>
2026-07-19 15:09:56 +0000

    wip

--- modules/hosts/theseus/configuration.nix
+++ modules/hosts/theseus/configuration.nix
@@ -8,7 +8,7 @@ in {
   flake.modules.nixos.${hostname} = {pkgs, ...}: {
     imports = with inputs.self.modules.nixos; [
       system-desktop
-      systemd-boot
+      grub
       # bluetooth
     ];
 
