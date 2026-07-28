e8ab6abb08e729ad68582860b2b510eacda0cf0a
Alan Panayotov <alanpanayotov@gmail.com>
2026-07-28 10:14:48 +0000

    podman tweaks

--- modules/system/settings/podman.nix
+++ modules/system/settings/podman.nix
@@ -7,6 +7,9 @@
         dockerCompat = true;
         defaultNetwork.settings.dns_enabled = true; # Required for containers under podman-compose to be able to talk to each other.
       };
+      oci-containers = {
+        backend = "podman";
+      };
     };
   };
 }
