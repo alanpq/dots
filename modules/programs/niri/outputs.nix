e19b4f70ed1d07396dd383554b989f924fcfe485
Alan Panayotov <alanpanayotov@gmail.com>
2026-09-17 15:51:32 +0100

    niri: support output rotation

--- modules/programs/niri/outputs.nix
+++ modules/programs/niri/outputs.nix
@@ -1,12 +1,23 @@
 {
   flake.modules.hjem.niri = {lib, ...}: {
     rum.desktops.niri = let
+      realWidth = monitor: (
+        if (builtins.elem (monitor.transform or "n") ["90" "270"])
+        then monitor.height
+        else monitor.width
+      );
+      realHeight = monitor: (
+        if (builtins.elem (monitor.transform or "n") ["90" "270"])
+        then monitor.width
+        else monitor.height
+      );
       place = let
         defaultMonitor = {
           position = {
             x = 0;
             y = 0;
           };
+          transform = "normal";
           rate = 60;
         };
         mkPlacer = f: output: monitor: self:
@@ -24,29 +35,34 @@
       in {
         leftOf = mkPlacer (
           self: other: {
-            x = other.position.x - self.width;
-            inherit (other.position) y;
+            x = other.position.x - realWidth self;
+            y = other.position.y + self.position.y;
           }
         );
         rightOf = mkPlacer (
           self: other: {
-            x = other.position.x + self.width;
-            inherit (other.position) y;
+            x = other.position.x + realWidth self;
+            y = other.position.y + self.position.y;
           }
         );
       };
 
       outputs = lib.fix (self: {
-        "DP-6" = {
+        "DP-4" = {
           width = 1920;
           height = 1080;
           rate = "144.001";
+          transform = "90";
         };
         "DVI-D-1" =
-          place.rightOf "DP-6" {
+          place.rightOf "DP-4" {
             width = 1920;
             height = 1080;
             rate = "144.001";
+            position = {
+              x = 0;
+              y = 520;
+            };
             # rate = "60";
 
             primary = true;
@@ -65,6 +81,7 @@
             output "${output}" {
                 mode "${toString cfg.width}x${toString cfg.height}@${(cfg.rate or "60")}"
                 position x=${toString pos.x} y=${toString pos.y}
+                transform "${cfg.transform or "normal"}"
             }
           ''
         )
